6d749b98c405a3596e6ad8074f24d249f6f7134d
Viktoria <Podisens@mail.ru>
2023-04-03 00:22:30 +0300

    Added_a_landing_page

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const completeProjectsSwiper = new Swiper('.swiper-complete-projects', {
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  const fantasiesSwiper = new Swiper('.swiper-fantasies', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
